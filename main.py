36973747db0e96eba2d315a1be0e9533f6f8812a
Ayush Deolasee <ayushdeolasee@Ayushs-MacBook-Pro.local>
2024-01-03 23:41:01 +0530

    Refactoring the code

--- main.py
+++ main.py
@@ -5,11 +5,26 @@ def tit_for_tat(opponent_previous_decision):
 
 def joss(opponent_previous_decision):
     true = random.choices([True, False], weights=(10,90))
-    print(true)
     if true[0] == False:
         return opponent_previous_decision
     else:
         return random.choices(['C', 'D'])
+
+def scoring(player1_decision, player2_decision):
+    player1_points = 0
+    player2_points = 0 
+    if player1_decision == 'C' and player2_decision == 'C':
+            player1_points += 3
+            player2_points += 3
+    if player1_decision == 'D' and player2_decision == 'C':
+            player1_points += 5
+    if player1_decision == 'C' and player2_decision == 'D':
+            player2_points += 5
+    if player1_decision == 'D' and player2_decision == 'D':
+            player1_points += 1
+            player2_points += 1
+    print(player1_points, player2_points)
+    return (player1_points, player2_points)
 
 def game_master():
     number_of_rounds = 0
@@ -25,39 +40,28 @@ def game_master():
             player1_decisions.append('C')
             player2 = joss('C')
             player2_decisions.append('C')
-            if player1 == 'C' and player2 == 'C':
-                player1_points += 3
-                player2_points += 3
-            if player1 == 'D' and player2 == 'C':
-                player1_points += 5
-            if player1 == 'C' and player2 == 'D':
-                player2_points += 5
-            if player1 == 'D' and player2 == 'D':
-                player1_points += 1
-                player2_points += 1
+            player1_new_points, player2_new_points = scoring(player1, player2)
+            player1_points += player1_new_points
+            player2_points += player2_new_points
             number_of_rounds += 1
         else:
             player1 = tit_for_tat(player2_decisions[-1])
             player2 = joss(player1_decisions[-1])
             player1_decisions.append(player1[0])
             player2_decisions.append(player2[0])
-            if player1 == 'C' and player2 == 'C':
-                player1_points += 3
-                player2_points += 3
-            if player1 == 'D' and player2 == 'C':
-                player1_points += 5
-            if player1 == 'C' and player2 == 'D':
-                player2_points += 5
-            if player1 == 'D' and player2 == 'D':
-                player1_points += 1
-                player2_points += 1
+
+            player1_new_points, player2_new_points = scoring(player1, player2)
+            player1_points += player1_new_points
+            player2_points += player2_new_points
             number_of_rounds += 1 
             
     print(f"Player 1 Points: {player1_points} | Player 1 Decisions: {player1_decisions} | Player 2 Points: {player2_points} | Player 2 Decisions: {player2_decisions}")
     if player1_points > player2_points:
         return "Player 1 Wins"
-    else:
+    elif "Player 2 Wins":
         return "Player 2 Wins"
+    else:
+         return "Draw"
         
 if __name__ == "__main__":
     n = 0
